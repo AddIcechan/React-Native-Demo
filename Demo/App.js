27f3be23eeaa75a77631f5b298a10d350a96dea0
AddIce <addice.ch@gmail.com>
2018-06-09 15:46:30 +0800

    完成首页框架

--- Demo/App.js
+++ Demo/App.js
@@ -10,14 +10,16 @@ import {
   Text,
   View,
   ListView,
+  FlatList,
   TouchableOpacity,
+  Image,
+  Alert
 } from 'react-native';
 
 import { createStackNavigator } from 'react-navigation';
 
 const dataList = [
-  "豆瓣影院热映",
-  "天气",
+  {key: 'a' ,title:"豆瓣影院热映"},
 ];
 
 class HomeScreen extends Component {
@@ -38,31 +40,39 @@ class HomeScreen extends Component {
 
   render(){
     return(
-      <ListView
-        dataSource={this.state.dataSource}
+      <FlatList
+      // data={[{key: 'a'}, {key: 'b'}]}
+  // renderItem={({item}) => <Text>{item.key}</Text>}
+        data={dataList}
+        renderItem={this._renderItem}
+        // keyExtractor={this._keyExtractor}
+        ItemSeparatorComponent={this._ItemSeparatorComponent}
         renderRow={this._renderRow}
-        renderSeparator={this._renderSeparator}>
-        </ListView>
+        renderSeparator={this._renderSeparator}
+        />
     );
   }
 
-  _renderRow = (rowData, sectionID, rowID, highlightRow) => {
-          let rowIndex = Number(rowID) + 1;
-          return (
-            <TouchableOpacity onPress={ () => {
-                alert(rowIndex+rowData);
-            } }>
-              <Text style={styles.rowTitle}> {rowIndex}. {rowData} </Text>
-            </TouchableOpacity>
-          );
+  // _keyExtractor = (item, index) => item.ID
 
-  } ;
+  _renderItem = ({item, index}) => {
+    let rowIndex = Number(index) + 1;
+    return (
+      <TouchableOpacity style={styles.container} 
+      onPress={() => { Alert.alert(item.key, item.title) } }>
+        <Text style={styles.rowTitle}> {rowIndex}. {item.title} </Text>
+        <Image style={styles.rowIndicator} source={require('./sources/indicator_right.png')} />
+      </TouchableOpacity>
+    );
+  };
 
-  _renderSeparator = (sectionID, rowID, adjacentRowHighlighted) => {
+  _ItemSeparatorComponent = () => {
       return <View style={styles.separator}/>;
   };
 
-  
+  _onPress = (title:string, index) => {
+    alert(index+title);
+  };
   
 
 }
@@ -78,7 +88,19 @@ const styles = StyleSheet.create({
   separator: {
     backgroundColor: 0x313131,
     height:1,
-  }
+  },
+
+  rowIndicator: {
+      marginRight: 20,
+  },
+
+  container: {
+      flex: 1,
+      flexDirection: 'row',
+      backgroundColor: 'white',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+  },
 
 });
 
