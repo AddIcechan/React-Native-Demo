5a655240a359a87b7186ba3053fb428990cc4553
AddIce <addice.ch@gmail.com>
2018-06-14 20:51:29 +0800

    加入 DBMovieRegion.js

---
+++ Demo/app/day01/DBMovieRegion.js
@@ -0,0 +1,25 @@
+
+import React, { Component } from 'react';
+import {  
+    View, 
+    Text, 
+    
+} from 'react-native';
+
+import { cities } from "../day01/Cities";
+
+export default class DBMovieRegion extends Component {
+
+    static navigationOptions = {
+        title: "地区",
+    }
+
+    render() {
+
+        return (
+        <View>
+            <Text> textInComponent </Text>
+        </View>
+        );
+    }
+}
